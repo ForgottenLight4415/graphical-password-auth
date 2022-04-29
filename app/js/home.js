fb4954b9f50a3328061bcf07357618e14bc9d850
tanvisalian19 <104268692+tanvisalian19@users.noreply.github.com>
2022-04-29 17:02:23 +0530

    home page

---
+++ app/js/home.js
@@ -0,0 +1,42 @@
+module.exports = () => {
+    if (document.body.classList.contains('homepage') === false) {
+        return;
+    }
+    // Initial Members Request
+    const tableBody = document.querySelector('#users tbody')
+    requests.getAllMembers().then(res => {
+        const members = res.data
+        for (const member of members) {
+            const tableRow = document.createElement('tr')
+            tableRow.innerHTML = `
+            <div class="d-flex flex-column justify-content-center">
+            <h6 class="mb-0 text-xs">${member.name}</h6>
+            <p class="text-xs text-secondary mb-0">${member.email}</p>
+            </div>
+            `
+            tableBody.appendChild(tableRow)
+        }
+
+    })
+    // Add New Member Form Handling
+    const addMemberFormNameEl = document.getElementById('addMemberFormName');
+    const addMemberFormEmailEl = document.getElementById('addMemberFormEmail');
+
+    formEl.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const name = addMemberFormNameEl.value
+        const email = addMemberFormEmailEl.value
+        const data = {
+            name,
+            email,
+        }
+        requests.addMember(data)
+            .then(() => {
+                window.location.reload()
+            })
+            .catch(() => {
+
+            })
+    })
+
+}
